f2d01b0e72f5349b59b188d8d50b8bcdc0e065ed
Tyson Erdmann <erdmatys001@alpinesd.org>
2025-11-04 09:08:18 -0700

    Minigame

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ function displayText() {
   paraGraph.style.color = "red";
   paraGraph.style.fontWeight = "bold";
 }
+
+let name = prompt("What's your name?");
+if (name !== "Tyrone" || name !== "tyrone") {
+  alert("Oh, I thought you wer're someone else...");
+}
+if (name != "") {
+  alert(`Hi ${name}!`);
+}
+
+let color = prompt("What's your favorite color?");
+
+if (name === "Tyrone" && color === "green") {
+  window.location.href = "adminConsole.html";
+}
+
+if (color === "green") {
+  alert("You're on to something...");
+} else if (color != "") {
+  alert("That's a nice color");
+}
+
+const numberInp = document.querySelector("inputBx");
+const guessBtn = document.querySelector("guessBtn");
+const attemptCnt = document.querySelector("numTry");
+let randomNumber = Math.floor(Math.random() * 10);
+
+guessBtn.addEventListener("click", displayText);
+
+function onGuess() {
+  attemptCnt++;
+  const inpValue = Number(numberInp.value);
+  if (inpValue === randomNumber) {
+    numberInp.disabled = true;
+  }
+}
